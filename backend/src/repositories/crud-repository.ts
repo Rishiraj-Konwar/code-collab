55f87a4e91c66d1067b2f713b3b6da6326f02389
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-01-29 12:20:52 +0530

    crud basics

--- backend/src/repositories/crud-repository.ts
+++ backend/src/repositories/crud-repository.ts
@@ -1,2 +1,16 @@
 import {StatusCodes} from "http-status-codes"
-
+import type { Model, ModelStatic } from "sequelize"
+export class CrudRepository {
+  public model: ModelStatic<Model>
+  constructor(model: ModelStatic<Model>){
+    this.model = model
+  }
+  async create(data: any): Promise<Model>{
+      const response = await this.model.create(data)
+      return response
+  }
+  async get(data: any) {
+    const response = await this.model.findByPk(data)
+    return response
+  }
+}
