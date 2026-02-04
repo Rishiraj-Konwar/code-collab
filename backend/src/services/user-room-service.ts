19ecd899f43c63a4eb93a39592c2fba0a88d574d
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-02-04 20:35:55 +0530

    reset the user room service

--- backend/src/services/user-room-service.ts
+++ backend/src/services/user-room-service.ts
@@ -10,4 +10,22 @@ export async function joinRoom(
   userId: string,
   slug: string,
 ): Promise<UserRoomInstance> {
+  const slugParts = slug.split("-");
+  const roomId = slugParts.pop()
+  const room = await roomRepository.get({
+    roomId: roomId
+  });
+  if (!room) {
+    throw new AppError("Room not found", StatusCodes.NOT_FOUND);
+  }
+  const response = await userRoomRepository.create({
+    data:{
+      userId: userId,
+      roomId: roomId
+    }
+  })
+  if (!response){
+    throw new AppError("Cannot join the room", StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+  return response
 }
