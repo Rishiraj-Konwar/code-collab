bce17b6f5c74a3e7eedb0363bdb905a2e99d4ea0
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-02-13 15:54:27 +0530

    add the user-routes

--- backend/src/routes/v1/user-routes.ts
+++ backend/src/routes/v1/user-routes.ts
@@ -5,3 +5,6 @@ import { userController } from "../../controllers"
 export const userRouter = express.Router()
 
 userRouter.get("/", authMiddleware.authenticateUser, userController.getUser)
+userRouter.patch("/", authMiddleware.authenticateUser, userController.updateUser)
+userRouter.patch("/change-password", authMiddleware.authenticateUser, userController.changePassword)
+userRouter.delete("/", authMiddleware.authenticateUser, userController.deleteUser)
